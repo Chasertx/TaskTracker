38aa7b422769305be7ef31e8c7aa0e9caeff14b8
Chasertx <laskylvet@yandex.com>
2024-08-18 10:23:08 -0500

    Setup Complete

--- LifeTracker/app/_layout.js
+++ LifeTracker/app/_layout.js
@@ -1,11 +1,47 @@
 /*
-Determines application structure and routing
+Determines application structure and routing 
+of components in the application
 */
 
 import { StyleSheet, Text, View } from 'react-native'
-import {Slot, Stack} from 'expo-router'
+import {Slot, SplashScreen, Stack} from 'expo-router'
+import { useFonts } from 'expo-font'
+import { useEffect } from 'react';
+
+//Prevents screen from auto hiding before assets are loaded.
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+    //getting all the fonts loaded
+    const [fontsLoaded, error] = useFonts({
+        //"FontName" : require ("/font/location")
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+      });
+
+    /*
+    UseEffect - allows us to perform actions
+    while a page is loading 
+    function callback executes when fontsLoaded
+    changes or there is an error
+    */
+    useEffect(() => {
+        if(error) throw error;
+
+        //Hides Native flashscreen immediately
+        if(fontsLoaded) SplashScreen.hideAsync();
+
+    }, [fontsLoaded, error])
+
+    if(!fontsLoaded && !error) return null;
+
     /*
     Stack = stack of individual screens
     Slot = Content area
--- LifeTracker/app/index.js
+++ LifeTracker/app/index.js
@@ -1,9 +1,11 @@
 /*
-Serves as a home page to navigate between various
-pages defined in separate files of the application.
+Serves as a entry point for the application:
+ * Starting point: It's the file that is loaded first when your app starts.
+ * Registering the app: It registers your main component with the native mobile platform.
+ * Connecting to native modules: It often handles the connection to native modules and APIs.
 */
 import { StatusBar } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 /*
@@ -13,24 +15,13 @@ StatusBar = zone at the top of the screen. Contains the time, wifi, cell network
 */
 const Home = () => {
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className= "text-3xl font-pblack">Home</Text>
             <StatusBar style="auto"/>
             <Link href= "/profile" style = {{color: 'blue'}}>Go to profile</Link>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-      display: 'flex',
-      flex: '1',
-      //Aligns horizontally
-      alignItems: 'center',
-      //Aligns vertically
-      justifyContent: 'center'
-    }
-    
-})
 
 export default Home;
